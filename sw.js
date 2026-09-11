a9d2383968f752cfe37688852a5336412f194794
richardfinardi <richard@consultoriarf.net>
2026-09-11 12:52:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,13 @@
-const CACHE = 'consultoria-rf-v4.9.12';
+const CACHE = 'consultoria-rf-v5.2.2';
 const APP_SHELL = [
   './app.html',
   './manifest.json',
-  './rf-icon-32-v4910.png',
   './rf-icon-192-v4910.png',
   './rf-icon-512-v4910.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).catch(console.warn)
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).catch(console.warn));
   self.skipWaiting();
 });
 
