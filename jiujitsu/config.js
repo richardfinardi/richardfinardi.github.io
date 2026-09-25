dff8520bd839447db90881fbfcfdc1bc02aba01c
richardfinardi <richard@consultoriarf.net>
2026-09-25 09:29:57 -0300

    Jiu-Jitsu app: conecta Apps Script backend

--- jiujitsu/config.js
+++ jiujitsu/config.js
@@ -1,4 +1,4 @@
 window.JJ_CONFIG = {
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbymMb73cD7fUv0dtMXgKKKj3fmt9qxgrNQLuZ5fUl0hngLFEiZGwiQZiCnZ_8-s2EE0/exec",
   SPREADSHEET_ID: "1fn-RSRPiq86bG7m84TeMUrhpGyCChaTNtngKL8Majj4"
 };
